ff2081892b12d34a54b373a30790ac019c3a983b
nicocasa27 <nicoelcrack27@gmail.com>
2023-09-13 09:23:39 -0600

    Area cmas

---
+++ Sesion 11/calcular_radio.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+const float PI  = 3.1416;
+
+
+int main()
+{
+ 
+ double r;
+ cout << "Radio;";
+ cin >> r;
+ cout << "Area:" << PI;
+
+}
